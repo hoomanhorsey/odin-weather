cdaa642d1a2c4853c8aee7e3c7c43035b8bc290c
Hermshy Mac <94696321+hoomanhorsey@users.noreply.github.com>
2024-09-17 10:14:49 +1000

    error messages for weather

--- javascript.js
+++ javascript.js
@@ -1,25 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
   // URL (required), options (optional)
-  async function getWeather(cityName) {
-    console.log(cityName);
+  async function getWeather(cityName, unit) {
+    try {
+      console.log(cityName);
 
-    const weatherResult = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=metric&key=845WSTLXENQ37FGANTMAYK5SM`,
-      { mode: "cors" }
-    );
-    const weatherData = await weatherResult.json();
+      const weatherResult = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=${unit}&key=845WSTLXENQ37FGANTMAYK5SM`,
+        { mode: "cors" }
+      );
 
-    console.log(weatherData);
-    console.log(weatherData.address);
-    console.log(weatherData.currentConditions.temp);
+      if (!weatherResult.ok) {
+        throw new Error(`${weatherResult.status}`);
+      }
+      const weatherData = await weatherResult.json();
 
-    const weatherDiv = document.getElementById("queryResult");
-    weatherDiv.innerHTML =
-      weatherData.currentConditions.temp +
-      "  " +
-      weatherData.address +
-      "  " +
-      weatherData.currentConditions.icon;
+      console.log(weatherData);
+      console.log(weatherData.address);
+      console.log(weatherData.currentConditions.temp);
+
+      const weatherDiv = document.getElementById("queryResult");
+      weatherDiv.innerHTML = `
+      City:   ${weatherData.address}, 
+      Temperature: ${weatherData.currentConditions.temp},
+      Conditions:
+      ${weatherData.currentConditions.icon}`;
+
+      console.log(` City: ${weatherData.address}, 
+      Temperature:
+      ${weatherData.currentConditions.temp},
+      Conditions:
+      ${weatherData.currentConditions.icon}`);
+    } catch (error) {
+      console.log(error);
+      console.error(
+        "I'm slorry I don't believe there is a city with that name. Could you please try and enter again?" +
+          error.status
+      );
+      alert(
+        "I'm slearry I don't believe there is a city with that name. Could you please try and enter again?: \n\n" +
+          error
+      );
+    }
   }
 
   const userForm = document.getElementById("userForm");
@@ -27,11 +48,10 @@ document.addEventListener("DOMContentLoaded", () => {
   userForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    alert(userForm);
-    alert(userForm.city.value);
+    const unit = userForm.unit.value;
 
     const cityName = userForm.city.value;
 
-    getWeather(cityName);
+    getWeather(cityName, unit);
   });
 });
